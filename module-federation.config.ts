bd23f8af98639bd80d9afbaaa202f265d03d26aa
Barry Mulrooney <barry.mulrooney@mulrooneydesign.ie>
2025-09-21 13:01:51 +0200

    add remote and ch-ch-changes

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -2,11 +2,16 @@ export const mfConfig = {
   name: 'mf_host',
   exposes: {},
   remotes: {
-    remoteHeader: 'mf_header@http://localhost:8081/mf-manifest.json',
+    remoteHeader:
+      'mf_header@https://modulefederationremotetest.netlify.app/mf-manifest.json',
   },
   shared: {
     react: { singleton: true },
-    'react-dom': { singelton: true },
-    'mf-pack': { singleton: true },
+    'react-dom': {
+      singleton: true,
+    },
+    'mf-pack': {
+      singleton: true,
+    },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import ReactDOM from 'react-dom/client';
 import { Header } from 'remoteHeader/Header';
+import React, { use, useContext } from 'react';
+import { ColorContext } from 'mf-pack';
 import './index.css';
 
-const App = () => (
-  <div className="container">
-    <Header />
-    <div>Name: mf-host</div>
-    <div>Framework: react-19</div>
-  </div>
-);
+const App = () => {
+  return (
+    <ColorContext.Provider value="orange">
+      <div className="container">
+        <Header />
+        <Logger />
+        <div>Name: mf-host</div>
+        <div>Framework: react-19</div>
+      </div>
+    </ColorContext.Provider>
+  );
+};
+
+function Logger() {
+  const color = useContext(ColorContext);
+  console.log('### color from host', color);
+  return null;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 
